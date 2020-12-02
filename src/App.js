7a4e98fe7287d857740e8b41faa7518b8819bbd0
ColdMillenium <andrewgerribrowning@gmail.com>
2020-12-02 15:56:43 -0500

    changed routing. Only shows main menu if signed in

--- src/App.js
+++ src/App.js
@@ -6,81 +6,39 @@ import Button from '@material-ui/core/Button';
 import logo from './logo.svg';
 import './App.css';
 import MainMenuPage from './components/MainMenuPage/MainMenuPage';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 
-export default class App extends Component {
-  constructor(props){
-    super(props);
-
-    this.state = {
-        showing: "CHAT"
-    }
-  }
+export default function App(){
+  const {confirmUserName, hasUserName} = useContext(ClientContext);
   
-  render() {
+
     return (
      
       <Switch>
-          <Route exact path="/" component={SignIn}/>
+          <Route exact path="/">{ ()=>{
+            if(hasUserName){
+              return<MainMenuPage></MainMenuPage>
+            }else{
+              return <SignIn></SignIn>
+            }
+          }}
+          </Route>
           <Route exact path="/SignIn" component={SignIn}/>
-          <Route exact path="/MainMenu" component={MainMenuPage}/>
+          <Route exact path="/MainMenu">{()=>{
+            if(hasUserName){
+              return<MainMenuPage></MainMenuPage>
+            }else{
+              return <SignIn></SignIn>
+            }
+          }} 
+          </Route>
       </Switch>
  
       
-    )
-  }
-  changeDisplay = val => {
-    let newState = {...this.state};
-    if(val === "CALC" || val === "CHAT" || val === "SETTINGS"){
-      newState = {...newState, showing: val}
-    }
-    this.setState(newState);
-    return newState;
-    
+    );
+  
 
-    
-}
-  display(){
-    if(this.state.showing === "CALC"){
-      return this.calcDisplay();
-    }else if(this.state.showing === "CHAT"){
-      return this.homeDisplay();
-    }else{
-      return this.settingDisplay();
-    }
-   
-  }
-
-  menuButton(val){
-    return <Button onClick={() => this.changeDisplay(val)} variant="contained" color="primary">
-    {val}
-  </Button>
-  }
-  calcDisplay() {
-      return <div>
-        {this.menuButton("CHAT")}
-        {this.menuButton("SETTINGS")}
-        <Calculator />
-      </div>;
-  }
-  homeDisplay(){
-    return <div>
-      {this.menuButton("CHAT")}
-      {this.menuButton("SETTINGS")}
-      {/* <Chat></Chat> */}
-      <SignIn></SignIn>
-      
-      </div>;
-  }
-  settingDisplay(){
-    return <div>
-      {this.menuButton("CHAT")}
-      {this.menuButton("CALC")}
-      
-      
-      </div>;
-  }
 }
   
    
