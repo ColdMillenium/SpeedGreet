95aa0263f5beadcf1437f3c93f2c211b0b0d1fb7
ColdMillenium <andrewgerribrowning@gmail.com>
2020-12-06 12:32:09 -0500

    Removed Bootstrap

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 import ClientContextProvider from './contexts/ClientContext'
+
 
 ReactDOM.render(
   <React.StrictMode>
