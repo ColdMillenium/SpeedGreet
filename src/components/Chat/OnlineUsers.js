1e7ed4e0048083a305d3531edd9313281ecb89ed
ColdMillenium <andrewgerribrowning@gmail.com>
2020-12-07 16:20:42 -0500

    Finished implimenting online users component

--- src/components/Chat/OnlineUsers.js
+++ src/components/Chat/OnlineUsers.js
@@ -1,9 +1,76 @@
-import React from 'react'
+import React ,{useContext} from 'react'
+import { makeStyles, useTheme} from '@material-ui/core/styles'
+import Typography from "@material-ui/core/Typography";
+import {ClientContext} from '../../contexts/ClientContext'
 
-export default function OnlineUsers() {
+export default function OnlineUsers(props) {
+    const theme = useTheme();
+    const useStyles = makeStyles({
+        activeUsersPanel: {
+            width: 300,
+            height: '100%',
+            borderRight: "1px solid #cddfe7"
+        },
+        title:{
+            margin: '10 0 0 0',
+            paddingLeft: 30,
+            borderBottom: '1px solid #cddfe7',
+            paddingBottom: 10
+        }
+      });
+    const { users, yourID } = useContext(ClientContext);
+    const callPeer = props;
+
     return (
-        <div>
-            
+        <div className="active-users-panel" id="active-user-container">
+        <div className="panel-title">
+            <Typography variant='h5'>Active Users</Typography>
+        </div>
+            <div>
+                {Object.keys(users).map(key => {
+                    if (key === yourID) {
+                        return null;
+                    }
+                    return (
+                        <button key={key} className="user" onClick={() => callPeer(key)}>Call {users[key]}</button>
+                    );
+                })}
+            </div>
         </div>
     )
+
+    
+//   .active-users-panel {
+//     width: 300px;
+//     height: 100%;
+//     border-right: 1px solid #cddfe7;
+//   }
+  
+//   .panel-title {
+//     margin: 10px 0 0 0;
+//     padding-left: 30px;
+//     font-weight: 500;
+//     font-size: 18px;
+//     border-bottom: 1px solid #cddfe7;
+//     padding-bottom: 10px;
+//   }
+  
+//   .active-user {
+//     padding: 10px 30px;
+//     border-bottom: 1px solid #cddfe7;
+//     cursor: pointer;
+//     user-select: none;
+//   }
+  
+//   .active-user:hover {
+//     background-color: #e8e9eb;
+//     transition: background-color 0.5s ease;
+//   }
+  
+//   .active-user--selected {
+//     background-color: #fff;
+//     border-right: 5px solid #65a9e5;
+//     font-weight: 500;
+//     transition: all 0.5s ease;
+//   }
 }
