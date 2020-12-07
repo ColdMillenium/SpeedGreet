0ac6732ac5d3b67b137a1e735dba119052f62408
ColdMillenium <andrewgerribrowning@gmail.com>
2020-12-07 14:37:16 -0500

    Replaced with new header component

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -6,6 +6,7 @@ import Button from '@material-ui/core/Button'
 import { makeStyles, useTheme} from '@material-ui/core/styles'
 import './Chat.css'
 import {ClientContext} from '../../contexts/ClientContext'
+import Header from '../Header/Header'
 
 
 
@@ -182,15 +183,7 @@ export default function Chat() {
     return (
             <div>
                 <div className={classes.hangout}>
-                    <header className="header">
-                    <div className="logo-container">
-                        <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/fb9cc1fd-4970-4659-b67c-e1628299afb8/dax8ad0-f7c9770d-d388-4d0f-85e9-19d48666749b.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvZmI5Y2MxZmQtNDk3MC00NjU5LWI2N2MtZTE2MjgyOTlhZmI4XC9kYXg4YWQwLWY3Yzk3NzBkLWQzODgtNGQwZi04NWU5LTE5ZDQ4NjY2NzQ5Yi5wbmcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.OC10dwE38B2UzZ42-53dMcFbe21_iHAhyaDH3rmh9zE" alt="doge logo" className="logo-img" />
-                        <h1 className="logo-text">
-                        Meet<span className="logo-highlight">&</span>Greet 
-                        </h1>
-                        <img src="https://www.freepnglogos.com/uploads/anime-face-png/bloodnhonor-photos-tumview-19.png" alt="doge logo" className="logo-img" />
-                    </div>
-                    </header>
+                    <Header></Header>
                     <div className="content-container">
                     <div className="active-users-panel" id="active-user-container">
                         <h3 className="panel-title">Active Users:</h3>
