649ecf1f902e6921f50383fd2e20a895960b2eef
ColdMillenium <andrewgerribrowning@gmail.com>
2020-12-07 14:38:33 -0500

    styling for header component + new imgs

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,15 +3,67 @@ import {ClientContext} from '../../contexts/ClientContext'
 import { makeStyles, useTheme} from '@material-ui/core/styles'
 
 export default function Header() {
+    const theme = useTheme();
+    const useStyles = makeStyles({
+        header: {
+          background: theme.colors.dark,
+          color: theme.colors.accent,
+          padding: "10px 40px",
+          boxShadow: "0px 3px 6px rgba(0, 0, 0, 0.1)",
+          display: "flex",
+          alignItems: "center"
+        
+        },
+        logoImg:{
+            width: 70,
+            height: 70,
+        },
+        logoContainer:{
+            display: "flex",
+            alignItems: "center"
+        },
+        logoText:{
+            marginLeft: 10,
+            marginRight: 10
+        }
+    });
+    const classes = useStyles();
     return (
-        <header className="header">
-        <div className="logo-container">
-            <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/fb9cc1fd-4970-4659-b67c-e1628299afb8/dax8ad0-f7c9770d-d388-4d0f-85e9-19d48666749b.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvZmI5Y2MxZmQtNDk3MC00NjU5LWI2N2MtZTE2MjgyOTlhZmI4XC9kYXg4YWQwLWY3Yzk3NzBkLWQzODgtNGQwZi04NWU5LTE5ZDQ4NjY2NzQ5Yi5wbmcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.OC10dwE38B2UzZ42-53dMcFbe21_iHAhyaDH3rmh9zE" alt="doge logo" className="logo-img" />
-            <h1 className="logo-text">
+        <header className={classes.header}>
+      
+            <img src="http://icons.iconarchive.com/icons/google/noto-emoji-smileys/1024/10041-winking-face-with-tongue-icon.png" 
+            alt="doge logo" 
+            className={classes.logoImg} />
+            <h1 className={classes.logoText}>
             Meet<span className="logo-highlight">&</span>Greet 
             </h1>
-            <img src="https://www.freepnglogos.com/uploads/anime-face-png/bloodnhonor-photos-tumview-19.png" alt="doge logo" className="logo-img" />
-        </div>
+            <img src="https://icons.iconarchive.com/icons/google/noto-emoji-smileys/128/10003-face-with-tears-of-joy-icon.png" alt="doge logo" className={classes.logoImg} />
+      
         </header>
     )
 }
+// .header {
+//     background-color: #ffffff;
+//     padding: 10px 40px;
+//     box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.1);
+//   }
+  
+//   .header > .logo-container {
+//     display: flex;
+//     align-items: center;
+//   }
+  
+//   .header > .logo-container > .logo-img {
+//     width: 70px;
+//     height: 70px;
+//     margin-right: 15px;
+//   }
+  
+//   .header > .logo-container > .logo-text {
+//     font-size: 26px;
+//     font-weight: 700;
+//   }
+  
+//   .header > .logo-container > .logo-text > .logo-highlight {
+//     color: #65a9e5;
+//   }
