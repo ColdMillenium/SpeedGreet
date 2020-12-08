8bc1674994eb1ad08a8c744a4e8c08761fdc6cbe
ColdMillenium <andrewgerribrowning@gmail.com>
2020-12-07 20:59:44 -0500

    made header an app bar
    - made zindex above the drawer
    - it's also fixed in place
    - you can apparently pull useStyles outside of the component like this...

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,36 +1,41 @@
 import React from 'react'
 import {ClientContext} from '../../contexts/ClientContext'
 import { makeStyles, useTheme} from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+const useStyles = makeStyles((theme)=> ({
+    appBar: {
+      background: theme.colors.dark,
+      color: theme.colors.accent,
+      padding: "10px 40px",
+      boxShadow: "0px 3px 6px rgba(0, 0, 0, 0.1)",
+      display: "flex",
+      alignItems: "center",
+      zIndex: theme.zIndex.drawer + 1, //I guess this makes it so it sits above the default z index of drawers.
+    
+    },
+    logoImg:{
+        width: 70,
+        height: 70,
+    },
+    logoContainer:{
+        display: "flex",
+        alignItems: "center"
+    },
+    logoText:{
+        marginLeft: 10,
+        marginRight: 10
+    }
+}));
 
 export default function Header() {
     const theme = useTheme();
-    const useStyles = makeStyles({
-        header: {
-          background: theme.colors.dark,
-          color: theme.colors.accent,
-          padding: "10px 40px",
-          boxShadow: "0px 3px 6px rgba(0, 0, 0, 0.1)",
-          display: "flex",
-          alignItems: "center"
-        
-        },
-        logoImg:{
-            width: 70,
-            height: 70,
-        },
-        logoContainer:{
-            display: "flex",
-            alignItems: "center"
-        },
-        logoText:{
-            marginLeft: 10,
-            marginRight: 10
-        }
-    });
+  
     const classes = useStyles();
     return (
-        <header className={classes.header}>
-      
+        <AppBar position="fixed" className={classes.appBar}>
+            {/* Toolbar makes all these items fit one one line */}
+            <Toolbar>
             <img src="http://icons.iconarchive.com/icons/google/noto-emoji-smileys/1024/10041-winking-face-with-tongue-icon.png" 
             alt="doge logo" 
             className={classes.logoImg} />
@@ -38,8 +43,9 @@ export default function Header() {
             Meet<span className="logo-highlight">&</span>Greet 
             </h1>
             <img src="https://icons.iconarchive.com/icons/google/noto-emoji-smileys/128/10003-face-with-tears-of-joy-icon.png" alt="doge logo" className={classes.logoImg} />
-      
-        </header>
+            </Toolbar>
+       
+        </AppBar>
     )
 }
 // .header {
