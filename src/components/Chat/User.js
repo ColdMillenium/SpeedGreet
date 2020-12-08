d38ff68fa27b3f133c74e6f4cd168507f8ee6356
ColdMillenium <andrewgerribrowning@gmail.com>
2020-12-07 20:19:15 -0500

    Removing button

--- src/components/Chat/User.js
+++ src/components/Chat/User.js
@@ -2,20 +2,24 @@ import React from 'react'
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles, useTheme} from '@material-ui/core/styles'
+import Button from '@material-ui/core/Button';
 
 export default function User(props) {
     const theme = useTheme();
     const useStyles = makeStyles({
-    
+        user:{
+            padding: 5,
+            margin: 2,
+            color: "white"
+        },
+
     });
     const classes = useStyles();
     const name = props.name;
     return (
-        <div>
-            <Grid container spacing={2} justify="center">
+            <Grid className={classes.user} container spacing={2} align="center" justify="center">
                 <Grid item><AccountCircleIcon/></Grid>
                 <Grid item>{props.name}</Grid> 
             </Grid>
-        </div>
     )
 }
