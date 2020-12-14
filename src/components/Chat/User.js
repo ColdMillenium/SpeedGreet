7466938bd068a1b4705e42711302178d00b13326
ColdMillenium <andrewgerribrowning@gmail.com>
2020-12-13 22:51:25 -0500

    Using my custum drop down menu instead of material UI

--- src/components/Chat/User.js
+++ src/components/Chat/User.js
@@ -6,6 +6,7 @@ import Button from '@material-ui/core/Button';
 import ListItem from '@material-ui/core/ListItem';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
+import DropDownMenu from '../Utils/DropDownMenu.js';
 
 export default function User(props) {
     const theme = useTheme();
@@ -13,15 +14,18 @@ export default function User(props) {
         user:{
             padding: 5,
             margin: 2,
-            color: "white"
+            color: "white",
+            width: '240px',
         },
 
     });
+    
     const classes = useStyles();
     const name = props.name;
     const callPeer = props.callPeer;
     const [anchorEl, setAnchorEl] = React.useState(null);
 
+    
     const handleUserClick = (event) => {
         setAnchorEl(event.currentTarget);//this target our user object so the menu knows where to display
     };
@@ -38,16 +42,21 @@ export default function User(props) {
         setAnchorEl(null);
     }
 
+    const items = [
+        {name:`Call ${name}`, onClick: handleCall},
+        {name:`Chat ${name}`, onClick: handleChat}
+    ]
+
     return (
-        <div>
-        <ListItem button onClick={handleUserClick} >
-            <Grid className={classes.user} container spacing={2} align="center" justify="center">
-                <Grid item><AccountCircleIcon/></Grid>
-                <Grid item>{props.name}</Grid> 
-            </Grid>
-            
-        </ListItem>
-            <Menu
+        <DropDownMenu items={items}>
+            <ListItem button onClick={handleUserClick} >
+                <Grid className={classes.user} container spacing={2} align="center" justify="center">
+                    <Grid item><AccountCircleIcon/></Grid>
+                    <Grid item>{props.name}</Grid> 
+                </Grid>
+                
+            </ListItem>
+            {/* <Menu
                 id="simple-menu"
                 anchorEl={anchorEl} //looks for this anchor to figure out where to display
                 keepMounted
@@ -57,8 +66,8 @@ export default function User(props) {
                 <MenuItem onClick={handleCall}>Invite {name} to call </MenuItem>
                 <MenuItem onClick={handleChat}>Invite {name} to chat </MenuItem>
     
-            </Menu>
-        </div>
+            </Menu> */}
+        </DropDownMenu>
             
     )
 }
