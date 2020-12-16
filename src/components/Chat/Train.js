fe2117b6aadd435c244652fbb2175d3618a6f6cf
ColdMillenium <andrewgerribrowning@gmail.com>
2020-12-15 23:58:28 -0500

    Showing chat class in train instead of video for now

--- src/components/Chat/Train.js
+++ src/components/Chat/Train.js
@@ -7,46 +7,7 @@ import OnlineUsers from './OnlineUsers';
 import Toolbar from '@material-ui/core/Toolbar';
 import IncomingCall from '../IncomingCall/IncomingCall';
 import styled ,{ withTheme} from 'styled-components';
-
-
-// const useStyles = makeStyles((theme)=>({
-//     hangout: {
-//       background: theme.colors.dark,
-//       color:theme.colors.light,
-//     },
-//     videoChatContainer: {
-//         padding: 0,
-//         postion: "relative",
-//         margin: 0,
-//         width: "100%",
-//         height: "100%"
-//     },
-//     talkInfo:{
-        
-//     },
-//     remoteVideo:{
-//         objectFit:"fill",
-//         width: "100%",
-//         height: "100%",
-//         margin: 0,
-//         madding:0,
-//     },
-//     localVideo:{
-//         position: "absolute",
-//         border: "1px solid " + theme.colors.accent,
-//         bottom: 0,
-//         right: 0,
-//         borderRadius: 5,
-//         width: 300,
-//         boxShadow: "0 3 6 rgba(0, 0, 0, 0.2)"
-//     },
-//     contentContainer: {
-//         width: '100%',
-//         height: "auto",
-//         display: "flex",
-//         overflow: "hidden",
-//       }
-//   }));
+import Chat from './Chat'
 
 
 const RemoteVideo = styled.video`
@@ -75,22 +36,18 @@ const GridLayout = styled.div`
     display: grid;
     grid-template-areas: 
     "users header"
-    "users video"
+    "users chat"
     "users chat" ;
     grid-template-columns: 240px calc(100vw - 240px);
-    grid-template-rows: 40px auto 25vh;
+    grid-template-rows: 40px auto 90px;
     overflow: hidden;
 `;
 const HeaderContainer = styled.div`
     grid-area: header;
 `;
+
 const ChatContainer = styled.div`
-    border: 2px solid blue;
     grid-area: chat;
-`;
-const VideoChatContainer = styled.div`
-    border: 2px solid blue;
-    grid-area: video;
 `;
 const UsersContainer = styled.div`
     grid-area: users;
@@ -186,12 +143,14 @@ export function Train(props) {
                 <UsersContainer>
                     <OnlineUsers users={users} callPeer={callPeer}></OnlineUsers>
                 </UsersContainer>   
-                <VideoChatContainer> 
+                {/* <VideoChatContainer> 
                     <IncomingCall></IncomingCall>
                     {partnerVideoWindow}
                     {userVideoWindow}
-                </VideoChatContainer>
-                <ChatContainer>TODO: Impliment chat</ChatContainer>
+                </VideoChatContainer> */}
+                <ChatContainer>
+                    <Chat></Chat>
+                </ChatContainer>
             </GridLayout>      
     )
 }
