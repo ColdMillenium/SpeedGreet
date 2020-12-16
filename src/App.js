30d5a822c6c361ee04bd93abf282938176ac8361
ColdMillenium <andrewgerribrowning@gmail.com>
2020-12-15 23:57:05 -0500

    Added styling for custom scroll bar

--- src/App.js
+++ src/App.js
@@ -47,6 +47,26 @@ const GlobalStyle = createGlobalStyle`
   p{
     font-family: 'Quicksand', sans-serif;
   }
+  /* Works on Firefox */
+  * {
+    scrollbar-width: thin;
+    scrollbar-color: blue orange;
+  }
+
+  /* Works on Chrome, Edge, and Safari */
+  *::-webkit-scrollbar {
+    width: 12px;
+  }
+
+  *::-webkit-scrollbar-track {
+    background: orange;
+  }
+
+  *::-webkit-scrollbar-thumb {
+    background-color: blue;
+    border-radius: 20px;
+    border: 3px solid orange;
+  }
 `;
 
     
