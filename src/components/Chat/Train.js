d106274c5a79e2193031a6cc047411dff6e24e13
ColdMillenium <andrewgerribrowning@gmail.com>
2021-03-23 14:20:57 -0400

    Moved Users Panel stuff outside Train page

--- src/components/Chat/Train.js
+++ src/components/Chat/Train.js
@@ -68,7 +68,7 @@ const SettingPanel = styled.div`
     align-items: center;
     background-color: #282C33;
     max-height: 100vh;
-    max-width: 60px;
+    /* max-width: 60px; */
     padding: 10px;
     
     
@@ -78,115 +78,6 @@ const SettingPanel = styled.div`
     }
 `;
 
-// const UsersPanel = styled.div`
-//     background-color: #303742;
-//     min-width: 300px;
-//     padding: 20px;
-//     display: flex;
-//     flex-direction: column;
-//     gap: 50px;
-
-// `;
-const SearchUsers = styled.div`
-    display: flex;
-  
-    
-    input{
-        width: 100%;
-        font-size: 18px;
-        background-color: #282C33;
-        border: none;
-        padding: 13px;
-        border-radius: 50px;
-        color: #929292;
-    }
-
-    input:focus{
-        outline: 0;
-
-    }
-    input:active{
-        border: none
-    }
-
-`;
-const OnlineUsersContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 20px;
-    height: 100%;
-    overflow-y: auto;
-`;
-const OnlineUser = styled.div`
-    display: flex;
-    flex-direction: row;
-    cursor: pointer;
-    transition: all 0.1s ease-in;
-    
-    color: #EFEFEF;
-    img{
-        height: 50px;
-        width: 50px;
-       
-        margin-right: 10px;
-    }
-    .info{
-        display: flex;
-        flex-direction: column;
-        justify-content: space-evenly;
-    }
-    .info.name{
-        font-family: 'Roboto';
-        font-size: 18px;
-        font-weight: 300;
-    }
-    &:hover{
-        opacity: 0.6;
-    }
-`;
-const Tags = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-
-    .tag{
-        font-size: 12px;
-        padding: 3px 6px;
-        margin: 0 3px;
-        border-radius: 50px;
-       
-    }
-    .tag.first{
-        background-color:#EB5757 ;
-    }
-    .tag.second{
-        background-color:#F2994A ;
-    }
-    .tag.third{
-        background-color:#6FCF97;
-    }
-    
-`;
-
-const Divider = styled.div`
-    height: 1px;
-    background-color: #929292;
-    width: 100%;
-`;
-const RandoCall = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    text-align: center;
-    gap: 10px;
-    .rando-header{
-        font-size: 32px;
-        font-family: 'Montserrat';
-        font-weight: bold;
-      
-    }
-
-`;
 
 
 export function Train(props) {
@@ -259,12 +150,7 @@ export function Train(props) {
     
     let incomingCall;
     if (receivingCall && !callAccepted) {
-        incomingCall = (
-        <div>
-            <h1>{caller} is calling you</h1>
-            <button onClick={acceptCall}>Accept</button>
-        </div>
-        )
+        return <IncomingCall></IncomingCall>
     }
     let notification = () =>{
         console.log("wtf dude "+ callAccepted);
@@ -301,12 +187,11 @@ export function Train(props) {
     return (
           
         <Layout>
-            {partnerVideoWindow}
-               {userVideoWindow}
             <SettingPanel>
                 {/* <img src={settingsBtn} alt=""/> */}
                 <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="sign-out-alt" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="svg-inline--fa fa-sign-out-alt fa-w-16 fa-3x"><path fill="currentColor" d="M497 273L329 441c-15 15-41 4.5-41-17v-96H152c-13.3 0-24-10.7-24-24v-96c0-13.3 10.7-24 24-24h136V88c0-21.4 25.9-32 41-17l168 168c9.3 9.4 9.3 24.6 0 34zM192 436v-40c0-6.6-5.4-12-12-12H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h84c6.6 0 12-5.4 12-12V76c0-6.6-5.4-12-12-12H96c-53 0-96 43-96 96v192c0 53 43 96 96 96h84c6.6 0 12-5.4 12-12z" class=""></path></svg>
                 <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="sliders-h" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="svg-inline--fa fa-sliders-h fa-w-16 fa-5x"><path fill="currentColor" d="M496 384H160v-16c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v16H16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h80v16c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16v-16h336c8.8 0 16-7.2 16-16v-32c0-8.8-7.2-16-16-16zm0-160h-80v-16c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v16H16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h336v16c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16v-16h80c8.8 0 16-7.2 16-16v-32c0-8.8-7.2-16-16-16zm0-160H288V48c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v16H16C7.2 64 0 71.2 0 80v32c0 8.8 7.2 16 16 16h208v16c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16v-16h208c8.8 0 16-7.2 16-16V80c0-8.8-7.2-16-16-16z" class=""></path></svg>
+                <incomingCall></incomingCall>
             </SettingPanel>
             <UsersPanel></UsersPanel>
             <MsgPanelContent 
@@ -315,6 +200,7 @@ export function Train(props) {
                 sendMessage={sendMessage}
                 rooms={rooms}
             ></MsgPanelContent>
+            
         </Layout>  
     )
 }
