2ddd1223fdcbbd84f1c125a68ec84190ad88189d
ColdMillenium <andrewgerribrowning@gmail.com>
2021-03-23 13:15:05 -0400

    deleted msgpanel that had already been refactored into a component

--- src/components/Chat/Train.js
+++ src/components/Chat/Train.js
@@ -187,188 +187,6 @@ const RandoCall = styled.div`
 
 `;
 
-const MsgPanel = styled.div`
-    
-    position: relative;
-    background-color: #213048;
-    min-width: 300px;
-    width: 100%;
-`;
-const MsgHistory = styled.div`
-    
-    margin: auto;
-    height: calc(100% -75px);
-    padding: 0 30px;
-
-`;
-const ReceivedMsg = styled.div`
-    height: fit-content;
-    width: fit-content;
-    .from{
-        display: flex;
-        align-items: center;
-        gap: 10px;
-        padding: 3px;
-    }
-    .name{
-        font-size: 24px;
-    }
-    .time{
-        font-size: 18px;
-        color: #929292;
-    }
-    .msg{
-        color: #213048;
-        background-color: #A3F0F0;
-        width: fit-content;
-        font-size: 18px;
-        padding: 20px;
-        border-radius: 0 30px 30px 30px;
-    }
-`;
-const SentMsg = styled(ReceivedMsg)`
-    margin: 0 0 0 auto;
-    .from{
-        justify-content: right;
-    }
-    .msg{
-        border-radius: 30px 0 30px 30px;
-        background-color: #9CE878;
-    }
-`;
-
-const MsgInput = styled.div`
-    position: absolute;
-    bottom: 0;
-    left:0;
-    right:0;
-    background: #EFEFEF;
-    height: 75px;
-  
-    padding: 0 10px 0 30px;
-    
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-
-
-
-    input{
-        color: #C4C4C4;
-        background: transparent;
-        border: none;
-        width: calc(100% - 40px);
-        
-    }
-    input:focus{
-        outline: 0;
-
-    }
-    input:active{
-        border: none
-    }
-    button{
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        padding: 0;
-        background: transparent;
-        border: none;
-        cursor: pointer;
-        transition: all 0.1s ease;
-        height: 100%;
-        width: 40px
-    }
-    button:active{
-        transform: scale(0.9);
-        border: none;
-    }
-    button img{
-        height: 40px; 
-    }
-`;
-
-function MsgPanelConent(props){
-    const [message, setMessage] = useState('');
-    let history = [];
-    console.log(props);
-
-  
-    const inputRef = createRef();
-    function handleInputChange(e){
-        if(inputRef.current!=null){
-            // inputRef.current = 
-            setMessage(inputRef.current.value);
-            console.log(message);
-        }
-    }
-
-    function handleEnter(e){
-        if(e.keyCode === 13){
-            console.log("enter zawarudo!")
-            props.sendMessage(message, props.chatUser);
-            inputRef.current.value = '';
-        }
-    }
-   if(props.chatUser === null || props.rooms[props.chatUser] === null){
-       return <MsgPanel >{props.currentRoom}</MsgPanel>
-   }else{
-       
-    //    const history = props.rooms[props.chatUser].history;
-       return(
-        <MsgPanel>
-            <MsgHistory>
-                <div> - Chat History with {props.users[props.chatUser]} - </div>
-                <ReceivedMsg>
-                    <div className="from">
-                        <div className="name">Cloe Fish</div>
-                        <div className="time"> @ 8:30pm</div>
-                    </div>
-                    <div className="msg">Dude...you're actually kind of an asshole</div>
-                </ReceivedMsg>
-                <SentMsg>
-                    <div className="from">
-                        <div className="name">John Doe</div>
-                        <div className="time"> @ 8:31pm</div>
-                    </div>
-                    <div className="msg">Hey, it was really nice to meet you</div>
-                </SentMsg>
-                {history.forEach((text, index)=>{
-                    return <div key={index}>{text.msg}</div>
-                })}
-            </MsgHistory>
-            <MsgInput>
-                <input 
-                    ref={inputRef}
-                    type="text"
-                    onKeyDown={(e)=> {handleEnter(e)} }
-                    onChange={(e)=>{handleInputChange(e)}} 
-                    placeholder="Type something here to send..."
-                />
-                <button>
-                    <img src={msgSendBtn} alt=""></img>
-                </button>
-                
-            </MsgInput>
-        </MsgPanel>
-       )
-   }
-}
-
-
-
-// const HeaderContainer = styled.div`
-//     grid-area: header;
-// `;
-
-// const ChatContainer = styled.div`
-//     grid-area: chat;
-// `;
-// const UsersContainer = styled.div`
-//     grid-area: users;
-//     overflow-x: hidden;
-// `;
-
 
 export function Train(props) {
     const {
@@ -480,7 +298,6 @@ export function Train(props) {
     }
 
     return (
-        //old stuff
             // <GridLayout>
             //     <HeaderContainer>
             //         <Header/>
@@ -488,11 +305,11 @@ export function Train(props) {
             //     <UsersContainer>
             //         <OnlineUsers users={users} callPeer={callPeer}></OnlineUsers>
             //     </UsersContainer>   
-            //     {/* <VideoChatContainer> 
+            //     <VideoChatContainer> 
             //         <IncomingCall></IncomingCall>
             //         {partnerVideoWindow}
             //         {userVideoWindow}
-            //     </VideoChatContainer> */}
+            //     </VideoChatContainer>
             //     <ChatContainer>
             //         <Chat></Chat>
             //     </ChatContainer>
@@ -500,6 +317,8 @@ export function Train(props) {
 
         //new stuff
         <Layout>
+            {partnerVideoWindow}
+               {userVideoWindow}
             <SettingPanel>
                 {/* <img src={settingsBtn} alt=""/> */}
                 <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="sign-out-alt" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="svg-inline--fa fa-sign-out-alt fa-w-16 fa-3x"><path fill="currentColor" d="M497 273L329 441c-15 15-41 4.5-41-17v-96H152c-13.3 0-24-10.7-24-24v-96c0-13.3 10.7-24 24-24h136V88c0-21.4 25.9-32 41-17l168 168c9.3 9.4 9.3 24.6 0 34zM192 436v-40c0-6.6-5.4-12-12-12H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h84c6.6 0 12-5.4 12-12V76c0-6.6-5.4-12-12-12H96c-53 0-96 43-96 96v192c0 53 43 96 96 96h84c6.6 0 12-5.4 12-12z" class=""></path></svg>
@@ -550,7 +369,10 @@ export function Train(props) {
                 users={users}
                 sendMessage={sendMessage}
                 rooms={rooms}
-            ></MsgPanelContent>
+            >
+                
+                boobs
+            </MsgPanelContent>
         </Layout>  
     )
 }
